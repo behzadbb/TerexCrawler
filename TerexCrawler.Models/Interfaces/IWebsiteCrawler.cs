16c4473ad2b5a748a16631b43ea68dd24ac98833
Behzad <Behzad@DESKTOP-T41IHOJ>
2019-09-10 02:31:02 +0430

    Add Digikala Helper

---
+++ TerexCrawler.Models/Interfaces/IWebsiteCrawler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerexCrawler.Models.Interfaces
+{
+    public interface IWebsiteCrawler : IDisposable
+    {
+        string WebsiteName { get; set; }
+        string WebsiteUrl { get; set; }
+        string GetPage(string url);
+        string GetComment(string url);
+    }
+}
---
+++ TerexCrawler.Services/DigikalaHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TerexCrawler.HttpHelper;
+using TerexCrawler.Models;
+using TerexCrawler.Models.Enums;
+using TerexCrawler.Models.Interfaces;
+
+namespace TerexCrawler.Services.Digikala
+{
+    public class DigikalaHelper : IWebsiteCrawler
+    {
+        public string WebsiteName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string WebsiteUrl { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        IHttpClientHelper client = new RestSharpHelper();
+        ILoger Logger = new LoggerHelper();
+        public string GetComment(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPage(string url)
+        {
+            var res = client.GetHttp(url);
+            if (res.Success)
+            {
+                return res.Content;
+            }
+            else
+            {
+                LogDTO log = new LogDTO()
+                {
+                    DateTime = DateTime.Now,
+                    Description = res.ExeptionErrorMessage,
+                    ProjectId = (int)ProjectNames.HttpHelper,
+                    Url = url,
+                    MethodName = "Digikala - GetPage",
+                    Title = "Get HTML Error"
+                };
+                Logger.AddLog(log);
+                return string.Empty;
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~DigikalaHelper()
+        // {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
